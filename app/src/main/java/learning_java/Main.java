9ab0cdaf63bc9560093620b415fc5347ada239f5
Arthurf <arthurf43213@gmail.com>
2025-08-04 19:25:11 -0300

    Operadores de atribuição e Logicos em Java

--- app/src/main/java/learning_java/Main.java
+++ app/src/main/java/learning_java/Main.java
@@ -29,3 +29,33 @@ public class Main {
         boolean status = true;      
     }
 }*/
+
+
+/*  Operadores de atribuição e Logicos
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        var scanner = new Scanner(System.in);
+        System.out.println("Quanto é 2 + 2");
+        var result = scanner.nextInt();
+        var isRight = result == 4;
+        System.out.printf("Voce acertou? ", isRight);
+        if(isRight) System.out.println("Parabens");
+
+        var scanner = new Scanner(System.in);
+        System.out.println("Quanto é 2 + 2");
+        var result = scanner.nextInt();
+        var isWrong = result != 4;
+        System.out.printf("Voce errou? ", !isWrong);
+
+        var scanner = new Scanner(System.in); 
+        System.out.println("Quantos anos vc tem?");
+        var age = scanner.nextInt();
+        System.out.println("Voce é emancipado?");
+        var isEmancipated = scanner.nextBoolean();
+        var canDrive = age >= 18 || isEmancipated && age >= 16;
+        System.out.printf("Voce pode dirigir? ", canDrive);
+    }
+}
+    */
