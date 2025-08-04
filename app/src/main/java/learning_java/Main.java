aa7fae68233f0c935a970fb824c50e9d1991eed5
Arthurf <arthurf43213@gmail.com>
2025-08-04 18:41:00 -0300

    Tipos primitivos do Java

--- app/src/main/java/learning_java/Main.java
+++ app/src/main/java/learning_java/Main.java
@@ -2,7 +2,7 @@ package learning_java;
 
 import java.util.Scanner;
 
-public class Main {
+/*public class Main {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
@@ -12,4 +12,20 @@ public class Main {
         int age = scanner.nextInt();
         System.out.println("Ola " + name + " voce tem " + age + " anos");
         } 
-}
+}*/
+
+
+/* Tipos primitivos
+public class Main {
+
+    public static void main(String[] args) {
+        byte number = 1;
+        short number2 = 1;
+        int number3 = 1;
+        long number4 = 1L;
+        float number5 = 1.1f;
+        double number6 = 1.1d;
+        char character = 'a';  
+        boolean status = true;      
+    }
+}*/
